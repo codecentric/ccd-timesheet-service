3d0dc5f141d49618fe05590878915863f879e1e4
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-01-16 11:49:30 +0100

    Create Trait for reading from DB

---
+++ src/main/scala/de/codecentric/ccdashboard/service/timesheet/db/DatabaseReader.scala
@@ -0,0 +1,24 @@
+package de.codecentric.ccdashboard.service.timesheet.db
+
+import java.util.Date
+
+import de.codecentric.ccdashboard.service.timesheet.data.model._
+import de.codecentric.ccdashboard.service.timesheet.messages.{TeamMembershipQueryResult, WorklogQueryResult}
+
+import scala.concurrent.Future
+
+trait DatabaseReader {
+  def getTeamById(id: String): Future[Team]
+
+  def getTeams(): Future[List[Team]]
+
+  def getIssueById(id: String): Future[Issue]
+  def getUserSchedules(username: String, from: Date, to: Date): Future[List[UserSchedule]]
+
+  def getWorklog(username: String, from: Option[Date], to: Option[Date]): Future[WorklogQueryResult]
+  def getUtilizationReport(username: String, from: Date, to: Date): Future[List[UserUtilization]]
+  def getUserSchedule(username: String, from: Date, to: Date): Future[List[UserSchedule]]
+
+  def getTeamMembership(username: String): Future[List[TeamMembershipQueryResult]]
+  def getUserByName(username: String): Future[Option[User]]
+}
