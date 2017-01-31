f7ee7f0bcb2b6146e735a0f3a7d26f39540886d2
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-01-31 11:13:07 +0100

    Add test suite for data provider actor

--- src/test/scala/de/codecentric/ccdashboard/service/timesheet/data/ingest/DataProviderActorSpec.scala
+++ src/test/scala/de/codecentric/ccdashboard/service/timesheet/data/ingest/DataProviderActorSpec.scala
@@ -9,12 +9,24 @@ import de.codecentric.ccdashboard.service.timesheet.BaseAkkaSpec
 import de.codecentric.ccdashboard.service.timesheet.data.access.DataProviderActor
 import de.codecentric.ccdashboard.service.timesheet.data.model.Worklogs
 import de.codecentric.ccdashboard.service.timesheet.db.{DatabaseReader, DatabaseWriter}
-import de.codecentric.ccdashboard.service.timesheet.messages.{ReportQueryAggregationType, UserQuery, UserReportQuery}
+import de.codecentric.ccdashboard.service.timesheet.messages._
 import org.scalamock.scalatest.MockFactory
 
 class DataProviderActorSpec extends BaseAkkaSpec with MockFactory {
 
+  //TODO setup actorRef only once
   "DataProviderActor" should {
+
+    "handle worklog queries" in {
+      val reader = stub[DatabaseReader]
+      val actorRef = TestActorRef(Props(new DataProviderActor(LocalDate.now(), reader)))
+      val query = WorklogQuery("john.doe", None, None)
+
+      actorRef ! query
+
+      (reader.getWorklog _).verify(query.username, query.from, query.to)
+    }
+
     "handle user queries" in {
       val reader = stub[DatabaseReader]
       val actorRef = TestActorRef(Props(new DataProviderActor(LocalDate.now(), reader)))
@@ -36,6 +48,58 @@ class DataProviderActorSpec extends BaseAkkaSpec with MockFactory {
 
       (reader.getTeamMembership _).verify(name)
     }
+
+    "handle issue queries" in {
+      val reader = stub[DatabaseReader]
+      val actorRef = TestActorRef(Props(new DataProviderActor(LocalDate.now(), reader)))
+
+      val id = "101"
+
+      actorRef ! IssueQuery(id)
+
+      (reader.getIssueById _).verify(id)
+    }
+
+    "handle single team membership queries" in {
+      val reader = stub[DatabaseReader]
+      val actorRef = TestActorRef(Props(new DataProviderActor(LocalDate.now(), reader)))
+
+      val teamId = 101
+
+      actorRef ! SingleTeamMembershipQuery(teamId)
+
+      (reader.getTeamMembers _).verify(teamId)
+    }
+
+    "handle all team memberships queries" in {
+      val reader = stub[DatabaseReader]
+      val actorRef = TestActorRef(Props(new DataProviderActor(LocalDate.now(), reader)))
+
+      actorRef ! AllTeamMembershipQuery
+
+      (reader.getTeamIds _).verify()
+    }
+
+    "handle employee queries" in {
+      val reader = stub[DatabaseReader]
+      val actorRef = TestActorRef(Props(new DataProviderActor(LocalDate.now(), reader)))
+
+      actorRef ! EmployeesQuery
+
+      (reader.getEmployees _).verify()
+    }
+
+    "handle team report queries" in {
+      val reader = stub[DatabaseReader]
+      val actorRef = TestActorRef(Props(new DataProviderActor(LocalDate.now(), reader)))
+      val teamId = 101
+
+      actorRef ! TeamReportQuery(teamId, None, None, ReportQueryAggregationType.MONTHLY)
+
+      (reader.getTeamById _).verify(teamId)
+    }
+
+
   }
 }
 
