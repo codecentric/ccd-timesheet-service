bbb6297722c44cf0b37ea67620316913ee0ec1c9
Björn Jacobs <bjoern.jacobs@codecentric.de>
2017-02-01 11:23:25 +0100

    Allow dashes in username

--- src/main/scala/de/codecentric/ccdashboard/service/timesheet/routing/CustomPathMatchers.scala
+++ src/main/scala/de/codecentric/ccdashboard/service/timesheet/routing/CustomPathMatchers.scala
@@ -4,6 +4,6 @@ package de.codecentric.ccdashboard.service.timesheet.routing
   * @author Björn Jacobs <bjoern.jacobs@codecentric.de>
   */
 object CustomPathMatchers {
-  val usernameMatcher = """[A-Za-z\.]+""".r
+  val usernameMatcher = """[A-Za-z\.\-]+""".r
   val issueIdMatcher = """[A-Za-z0-9]+""".r
 }
