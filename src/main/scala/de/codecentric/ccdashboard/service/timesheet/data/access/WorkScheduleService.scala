b18b4477f1e4e888448cf786ca5864447f3f992a
Björn Jacobs <bjoern.jacobs@codecentric.de>
2017-02-01 17:37:33 +0100

    Fixed calculation of targetHours, limited targetHours to personal max target, estimate vacation usage for future months

--- src/main/scala/de/codecentric/ccdashboard/service/timesheet/data/access/WorkScheduleService.scala
+++ src/main/scala/de/codecentric/ccdashboard/service/timesheet/data/access/WorkScheduleService.scala
@@ -1,6 +1,7 @@
 package de.codecentric.ccdashboard.service.timesheet.data.access
 
 import java.time.LocalDate
+import java.time.LocalDate.now
 import java.time.temporal.{ChronoUnit, TemporalAdjusters}
 import java.util.Date
 
@@ -19,35 +20,59 @@ class WorkScheduleService(fullYearSchedules: List[UserSchedule], fullYearReports
   final private val startOfYear = LocalDate.ofYearDay(year, 1)
   final private val endOfYear = startOfYear.`with`(TemporalAdjusters.lastDayOfYear())
 
-  final val userStartOfYear = employeeSince.getOrElse(startOfYear.asUtilDate)
+  final val userStartOfYear: Date = employeeSince.getOrElse(startOfYear.asUtilDate)
   private val userMonthsThisYear = ChronoUnit.MONTHS.between(userStartOfYear.asLocalDate, endOfYear) +
     (if (userStartOfYear.asLocalDate.getDayOfMonth == 15) 0.5 else 1)
-  final val vacationDaysThisYear = (userMonthsThisYear * VACATION_DAYS_PER_YEAR / 12).round
+  final val vacationDaysThisYear: Long = (userMonthsThisYear * VACATION_DAYS_PER_YEAR / 12).round
 
-  final val workDaysThisYear = fullYearSchedules.count(_.requiredHours > 0)
+  final val workDaysThisYear: Int = fullYearSchedules.count(_.requiredHours > 0)
   private val userSchedules = fullYearSchedules.filter(s => s.workDate.after(userStartOfYear) || s.workDate.equals(userStartOfYear))
-  final val userWorkDaysThisYear = getWorkDaysFromUserSchedules(userSchedules)
-  final val userWorkDaysAvailabilityRate = userWorkDaysThisYear / workDaysThisYear
+  final val userWorkDaysThisYear: Double = getWorkDaysFromUserSchedules(userSchedules)
+  final val userWorkDaysAvailabilityRate: Double = userWorkDaysThisYear / workDaysThisYear
 
-  final val parentalLeaveDaysThisYear = fullYearReports.flatMap(_.parentalLeaveHours).sum / 8
-  final val targetHoursThisYear = (TARGET_HOURS_BASE * userWorkDaysAvailabilityRate) - (parentalLeaveDaysThisYear * 8 * 0.8)
+  final val parentalLeaveDaysThisYear: Double = fullYearReports.flatMap(_.parentalLeaveHours).sum / 8
+  final val targetHoursThisYear: Double = (TARGET_HOURS_BASE * userWorkDaysAvailabilityRate) - (parentalLeaveDaysThisYear * 8 * 0.8)
 
-  final val burndownHoursPerWorkday = targetHoursThisYear / (userWorkDaysThisYear - vacationDaysThisYear - parentalLeaveDaysThisYear)
-
+  final val burndownHoursPerWorkday: Double = targetHoursThisYear / (userWorkDaysThisYear - vacationDaysThisYear - parentalLeaveDaysThisYear)
 
   def getWorkScheduleUntil(endDate: Date): WorkScheduleEntry = {
     val reportsTillEndDate = fullYearReports.filter(_.day.before(endDate))
+
     val usedVacationDaysTillTodayInclusive = reportsTillEndDate.flatMap(_.vacationHours).sum / 8
     val usedParentalLeaveDaysTillTodayInclusive = reportsTillEndDate.flatMap(_.parentalLeaveHours).sum / 8
 
+    val remainingVacationDaysThisYear = VACATION_DAYS_PER_YEAR - usedVacationDaysTillTodayInclusive
+    val vacationDaysUsageEstimation = getVacationDaysUsageEstimation(remainingVacationDaysThisYear, endDate)
+
     val workDaysTillEndDate = getWorkDaysFromUserSchedules(userSchedules.filter(_.workDate.before(endDate)))
-    val targetHoursToday =
-      (workDaysTillEndDate - usedVacationDaysTillTodayInclusive - usedParentalLeaveDaysTillTodayInclusive) * burndownHoursPerWorkday
+    val remainingWorkDays = workDaysTillEndDate - usedVacationDaysTillTodayInclusive - usedParentalLeaveDaysTillTodayInclusive
+
+    val targetDays = remainingWorkDays - vacationDaysUsageEstimation
+
+    val targetHoursToday = List(targetDays * burndownHoursPerWorkday, targetHoursThisYear).min
 
     WorkScheduleEntry(endDate,
       usedVacationDaysTillTodayInclusive,
       usedParentalLeaveDaysTillTodayInclusive,
       targetHoursToday)
+  }
+
+  /**
+    * When we're looking at the past, we know the vacation usage for sure so we don't have to adjust.
+    * When we're looking at the future however, we have to guess the average usage of vacation days per month
+    *
+    * @return Estimated number of vacation days to take each month
+    */
+  private def getVacationDaysUsageEstimation(remainingVacationDaysThisYear: Double, endDate: Date) = {
+    val selectedMonth = endDate.asLocalDate.getMonthValue
+    val selectedYear = endDate.asLocalDate.getYear
+    val remainingMonths = List(12 - selectedMonth, 1).max
+
+    if (selectedMonth <= now().getMonthValue || selectedYear < now().getYear) {
+      0.0
+    } else {
+      remainingVacationDaysThisYear / remainingMonths
+    }
   }
 
   private def getWorkDaysFromUserSchedules(schedules: List[UserSchedule]) = {
