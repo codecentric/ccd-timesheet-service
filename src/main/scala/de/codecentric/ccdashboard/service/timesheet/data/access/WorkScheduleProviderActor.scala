96f2e158cb432eb8d87a8ea15a730011b1703a5d
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-02-07 09:45:03 +0100

    Remove DB Access from WorkscheduleActor

--- src/main/scala/de/codecentric/ccdashboard/service/timesheet/data/access/WorkScheduleProviderActor.scala
+++ src/main/scala/de/codecentric/ccdashboard/service/timesheet/data/access/WorkScheduleProviderActor.scala
@@ -6,12 +6,11 @@ import java.util.Date
 
 import akka.actor.{Actor, ActorLogging}
 import akka.pattern.pipe
-import com.google.common.reflect.TypeToken
 import de.codecentric.ccdashboard.service.timesheet.data.access.WorkScheduleProviderActor.WorkScheduleQuery
-import de.codecentric.ccdashboard.service.timesheet.data.model.{UserSchedule, UserUtilization}
+import de.codecentric.ccdashboard.service.timesheet.data.model.UserSchedule
+import de.codecentric.ccdashboard.service.timesheet.db.DatabaseReader
 import de.codecentric.ccdashboard.service.timesheet.messages._
 import de.codecentric.ccdashboard.service.timesheet.util.DateConversions._
-import io.getquill.{CassandraAsyncContext, CassandraContextConfig, SnakeCase}
 
 import scala.concurrent.Future
 
@@ -23,26 +22,9 @@ object WorkScheduleProviderActor {
 /**
   * Created by tbinias on 22.12.16.
   */
-class WorkScheduleProviderActor(cassandraContextConfig: CassandraContextConfig) extends Actor with ActorLogging {
+class WorkScheduleProviderActor(dbReader: DatabaseReader) extends Actor with ActorLogging {
 
-  lazy val ctx = new CassandraAsyncContext[SnakeCase](cassandraContextConfig)
   import context.dispatcher
-  import ctx._
-
-  private val stringToken = TypeToken.of(classOf[String])
-  private val dateToken = TypeToken.of(classOf[java.util.Date])
-
-  implicit class DateRangeFilter(a: Date) {
-    def >(b: Date) = quote(infix"$a > $b".as[Boolean])
-
-    def >=(b: Date) = quote(infix"$a >= $b".as[Boolean])
-
-    def <(b: Date) = quote(infix"$a < $b".as[Boolean])
-
-    def <=(b: Date) = quote(infix"$a <= $b".as[Boolean])
-
-    def ==(b: Date) = quote(infix"$a = $b".as[Boolean])
-  }
 
   def receive: Receive = {
     case WorkScheduleQuery(username: String, year: Option[Int]) =>
@@ -51,8 +33,8 @@ class WorkScheduleProviderActor(cassandraContextConfig: CassandraContextConfig) 
       val startOfYear = LocalDate.ofYearDay(year.getOrElse(LocalDate.now().getYear), 1)
       val endOfYear = startOfYear.`with`(TemporalAdjusters.lastDayOfYear())
       val resultFuture = for {
-         fullYearSchedules <- ctx.run(userSchedule(username, startOfYear.asUtilDate, endOfYear.asUtilDate))
-         fullYearReports <- ctx.run(userReport(username, startOfYear.asUtilDate, endOfYear.asUtilDate))
+         fullYearSchedules <- dbReader.getUserSchedules(username, startOfYear.asUtilDate, endOfYear.asUtilDate)
+         fullYearReports <- dbReader.getUtilizationReport(username, startOfYear.asUtilDate, endOfYear.asUtilDate)
          employeeStartDates <- getTeamMembershipStartDates(username)
       } yield {
          val workScheduleService = new WorkScheduleService(fullYearSchedules, fullYearReports,
@@ -98,24 +80,9 @@ class WorkScheduleProviderActor(cassandraContextConfig: CassandraContextConfig) 
     schedules.map(_.requiredHours).sum / 8
   }
 
-  def userSchedule(username: String, from: Date, to: Date): Quoted[Query[UserSchedule]] = {
-    query[UserSchedule]
-      .filter(_.username == lift(username))
-      .filter(_.workDate >= lift(from))
-      .filter(_.workDate <= lift(to))
-  }
-
-  def userReport(username: String, from: Date, to: Date): Quoted[Query[UserUtilization]] = {
-    query[UserUtilization]
-      .filter(_.username == lift(username))
-      .filter(_.day >= lift(from))
-      .filter(_.day <= lift(to))
-  }
-
   def getTeamMembershipStartDates(username: String): Future[List[Date]] = {
-    val result = ctx.executeQuery(s"SELECT date_from FROM team_member WHERE name = '$username' ALLOW FILTERING;",
-      extractor = row => row.get(0, dateToken))
+    val result = dbReader.getUserTeamMembershipDates(username)
 
-    result.map(_.filterNot(_ == null))
+    result.map(_.map(_.dateFrom.getOrElse(new Date())))
   }
 }
